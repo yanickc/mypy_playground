00d3a66db900f2424657cefce36a381c0fe93dfc
Yanick Chenard <yanick.chenard@elementai.com>
2018-04-20 18:45:10 -0400

    add ignore line example

--- examples/ignoring_stuff.py
+++ examples/ignoring_stuff.py
@@ -1,11 +1,8 @@
-
-
 from Crypto import Random
 
-
-Random.get_random_bytes(5) # type: ignore
+Random.get_random_bytes(5)  # type: ignore
 # or that would work
-#Random.new().get_random_bytes(5)
+# Random.new().get_random_bytes(5)
 
 # Without the ignore, we get:  error: Module has no attribute "get_random_bytes"
 # Due to the way the Random module declares its exports
